9e6da5e6c0abb85b72cfccd0da8576d46af3d8f7
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-08-27 23:46:03 +0200

    added the routes to the endpoints

--- index.js
+++ index.js
@@ -1,42 +1,70 @@
 const express = require("express");
-const app = express();
-const morgan = require("morgan");
-
-app.use(morgan("common"));
-
 const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+const app = express();
 
 app.use(bodyParser.json());
-app.use(methodOverride());
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
-let topMovies = [
+let movies = [
   {
     title: "Inglourious Basterds",
     author: "Quentin Tarantino",
   },
+  {
+    title: "John Wick: Chapter 4",
+    author: "Chad Stahelski",
+  },
+  {
+    title: "Fall",
+    author: "Scott Mann",
+  },
 ];
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my app!");
-});
-
+// Gets the list of data about ALL Movies
 app.get("/movies", (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
 
-app.use(express.static("public"));
+// Gets the data about a single movie, by title
+app.get("/movies/:Title", (req, res) => {
+  res.send("Data about a single movie");
+});
+
+// Get data about a genre by genre's name
+app.get("/movies/genre/:genreName", (req, res) => {
+  res.send("Get data about a genre by genre's name");
+});
+
+// Get data about a director by director's name
+app.get("/movies/directors/:directorName", (req, res) => {
+  res.send("Get data about a director by director's name");
+});
+
+// Add a new user
+app.post("/users", (req, res) => {
+  res.send("Create a new User");
+});
+
+// Update user info
+app.put("/users/:Username", (req, res) => {
+  res.send("Update a User");
+});
+
+//Add a movie to user favorite list
+app.post("/users/:Username/movies/:movie", (req, res) => {
+  res.send("Add a movie to user favorite list");
+});
+
+// Remove a movie from user favorite list
+app.delete("/users/:Username/movies/:movie", (req, res) => {
+  res.send("Remove a movie from user favorite list");
+});
+
+// Delete user registration
+app.delete("/users/:Username", (req, res) => {
+  res.send("Delete user registration");
+});
 
 app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
+  console.log("Your app is listening on port 8080");
 });
