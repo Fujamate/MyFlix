5a47d5eb9e2fbdbb6617ef1099c6cef5f7b292e5
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-09-18 13:16:11 +0200

    added env package

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Models = require("./models.js");
 const { check, validationResult } = require("express-validator");
+require("dotenv").config();
+
+const Models = require("./models.js");
 
 const Movies = Models.Movie;
 const Users = Models.User;
